1bf9ecbc7120c9c409c2c38f29c1a427cb3e8339
Daan <49617687+DaanDH12@users.noreply.github.com>
2021-08-24 12:11:49 +0200

    Updated site
    - Extra Tabs
    - Removed Lorem Ipsum

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-         if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        } else{
-            $('.navbar').removeClass("sticky"); 
-            
+        } else {
+            $('.navbar').removeClass("sticky");
+
         }
     })
     // toggle menu navbar script
 
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+});
+
+// typing animation script
+var typed = new Typed(".animatie", {
+    strings: ["Student", "Designer", "Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".animatie2", {
+    strings: ["Student", "Designer", "Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
