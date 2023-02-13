70c50de7f488d967e56bfec2d78ee90345decb14
DaanDH12 <daanhoning@kpnmail.nl>
2023-02-13 11:33:43 +0100

    Trying Wordpress on daanhoning.nl

---
+++ wp-content/themes/saasify/patterns/feature-intro-text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Feature Intro Text
+ * Slug: feature-intro-text
+ * Categories: feature
+ * 
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","right":"var:preset|spacing|xs","left":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--xs);padding-right:var(--wp--preset--spacing--xs);padding-bottom:var(--wp--preset--spacing--xs);padding-left:var(--wp--preset--spacing--xs)"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center">One easy to use dashboard to manage your team</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"}}}} -->
+<p class="aligncenter has-text-align-center aligncenter" style="margin-top:var(--wp--preset--spacing--xs)">Saasify offers a range of tools and features to help businesses run and grow their SaaS operations efficiently</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
