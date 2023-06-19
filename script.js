22d9eafb5ce777b07c530aef72fce0d520f05dff
DaanDH12 <daanhoning@kpnmail.nl>
2023-06-19 18:00:53 +0200

    changes for working link

--- script.js
+++ script.js
@@ -7,32 +7,30 @@ function loadSheetsAPI() {
   function fetchSheetData() {
     gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: '1p8Uc3wcJXZXixLIjaPooNhdArAacYIAZ7idmfXRFnWk', // Vervang met je eigen Spreadsheet ID
-      range: 'B:B', // Vervang met het bereik van de kolom waarin de 'nee'-waarden staan
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      }
+      range: 'Sheet1!B:B' // Vervang met het bereik van de kolom waarin de 'ja'/'nee'-waarden staan
     }).then(function(response) {
       var data = response.result.values;
       var rowCount = data.length;
   
-      var yesCount = 0;
+      var noCount = 0;
       for (var i = 0; i < rowCount; i++) {
         if (data[i][0] === 'nee') {
-          yesCount++;
+          noCount++;
         }
       }
+  
       var progressBar = document.querySelector('.progress-bar');
-      if (yesCount >= 6) {
+      if (noCount === 6) {
         progressBar.style.width = '100%';
+        progressBar.style.backgroundColor = 'green';
       } else {
-        progressBar.style.width = (yesCount / 6) * 100 + '%';
+        progressBar.style.width = (noCount / 6) * 100 + '%';
+        progressBar.style.backgroundColor = '#ffcc00';
       }
     }, function(reason) {
-      console.error(`Error: ${reason.result.error.message}`);
+      console.error('Error: ' + reason.result.error.message);
     });
   }
-
-  
   
   // Initialisatie van de Google Sheets API
   gapi.load('client', function() {
@@ -40,4 +38,5 @@ function loadSheetsAPI() {
       apiKey: 'AIzaSyDcpFXMiROdpf7cKUYoI8zh2F3-mJKZO_c' // Vervang met je eigen API-sleutel
     }).then(loadSheetsAPI);
   });
+  
   
