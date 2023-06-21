657b6913458360f985d197e24ce50fb232a84452
DaanDH12 <daanhoning@kpnmail.nl>
2023-06-21 14:24:30 +0200

    Updated Script

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ function changeText(elementId, text) {
 // Functie om gegevens op te halen uit de Google Sheet
 function fetchSheetData() {
   gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '1p8Uc3wcJXZXixLIjaPooNhdArAacYIAZ7idmfXRFnWk', // Vervang met je eigen Spreadsheet ID
-    range: 'B:B' // Vervang met het bereik van de kolom waarin de 'ja'/'Nee'-waarden staan
+    spreadsheetId: '1p8Uc3wcJXZXixLIjaPooNhdArAacYIAZ7idmfXRFnWk',
+    range: 'B:B'
   }).then(function(response) {
     var data = response.result.values;
     var rowCount = data.length;
 
     var textFragments = ['Elke keer als je een avond niet gedronken hebt, vult de progressbar hieronder zich! Als die helemaal vol zit zul je rijkelijk beloond worden Tim!', 'Lekker bezig Timmetje, al 2 avonden op rij niet gedronken! Je weet wat dat betekent.. De beloning komt dichterbij', 'Tim, wat een kanonskogel! Voel je je al fitter?', 'Tim, Tim, absolute animal! Ik ben ervan overtuigd dat je het gaat halen!', 'Gefeliciteerd Tim, jij hebt die beloning zeker verdiend! Je krijgt van mij een lekker pilsje binnenkort om dit te vieren!']; // Voeg hier de verschillende tekstfragmenten toe
-    var currentFragmentIndex = 1; // Index van het huidige tekstfragment
+    var currentFragmentIndex = 0; // Index van het huidige tekstfragment
 
     for (var i = 0; i < rowCount; i++) {
       if (data[i][0] === 'Nee') {
-        currentFragmentIndex = (currentFragmentIndex + 1) % textFragments.length; // Wissel naar het volgende tekstfragment
-        changeText('result-text', textFragments[currentFragmentIndex]); // Verander 'result-text' naar het id van het element waarin je de tekst wilt wijzigen
-        break; // Stop de lus zodra een 'Nee'-waarde wordt gevonden (als je maar één tekstfragment per keer wilt weergeven)
+        currentFragmentIndex = (currentFragmentIndex + 1) % textFragments.length; // Wisselt naar het volgende tekstfragment
+        changeText('result-text', textFragments[currentFragmentIndex]); // Verandert 'result-text' naar het id van het element waarin je de tekst wilt wijzigen
+        break; // Stopt de lus zodra een 'Nee'-waarde wordt gevonden (als je maar één tekstfragment per keer wilt weergeven)
       }
     }
 
@@ -45,9 +45,9 @@ function fetchSheetData() {
   });
 }
 
-// Initialisatie van de Google Sheets API
+// Google Sheets API Key
 gapi.load('client', function() {
   gapi.client.init({
-    apiKey: 'AIzaSyDcpFXMiROdpf7cKUYoI8zh2F3-mJKZO_c' // Vervang met je eigen API-sleutel
+    apiKey: 'AIzaSyDcpFXMiROdpf7cKUYoI8zh2F3-mJKZO_c'
   }).then(loadSheetsAPI);
 });
